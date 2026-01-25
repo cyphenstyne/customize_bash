f7741673caeeab61d7b94af6b1a622aa4473e31e
cyphenstyne <cyphenstyne@gmail.com>
2026-01-25 15:14:41 +0530

    Update .bash-customization.sh

--- .bash-customization.sh
+++ .bash-customization.sh
@@ -19,17 +19,24 @@ if [ -f ~/.git-prompt.sh ]; then
     source ~/.git-prompt.sh
 fi
 
-# 3. Configure Git Status
+# 3. Check if it's a container or not:
+if [ -n "$CONTAINER_ID" ]; then
+    box="$CONTAINER_ID"
+else
+    box="host"
+fi
+
+# 4. Configure Git Status
 export GIT_PS1_SHOWDIRTYSTATE=1
 export GIT_PS1_SHOWUNTRACKEDFILES=1
 export GIT_PS1_SHOWUPSTREAM="auto"
 
-# 4. Define Colors
+# 5. Define Colors
 RESET="\[\e[0m\]"
 GREEN="\[\e[32m\]"
 BLUE="\[\e[34m\]"
 CYAN="\[\e[36m\]"
 
-# 5. Build PS1 
-# add $CONTAINER_ID somewhere here, if setting this in container 
-export PS1='('$CYAN''$DISTRO_ICON''$RESET')'$GREEN'\u'$RESET'@'$BLUE'\h'$RESET'[\w]$(__git_ps1 " (%s)")\n '
+# 6. Build PS1
+# add $CONTAINER_ID somewhere here, if setting this in container
+export PS1='('$CYAN''$DISTRO_ICON' '$RESET'['$box'])'$GREEN'\u'$RESET'@'$BLUE'\h'$RESET'[\w]$(__git_ps1 " (%s)")\n '
